22101c041c26535e442eb0ffcb95975284f995ec
Ryan Meier <rameier@umich.edu>
2013-06-08 23:06:40 -0700

    Initial commit of a few basic files.

---
+++ letsmod_common/letsmod/LetsMod.java
@@ -0,0 +1,31 @@
+package letsmod;
+
+import letsmod.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.*;
+import cpw.mods.fml.common.event.*;
+
+@Mod( modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class LetsMod
+{
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        //Register Blocks/Items Here
+    }
+    
+    @Init
+    public void init(FMLInitializationEvent  event)
+    {
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        
+    }
+    
+    
+    
+}
---
+++ letsmod_common/letsmod/lib/Reference.java
@@ -0,0 +1,8 @@
+package letsmod.lib;
+
+public class Reference
+{
+    public static final String MOD_ID = "LetsMod";
+    public static final String MOD_NAME = "LetsMod";
+    public static final String VERSION = "0.0.1";
+}
